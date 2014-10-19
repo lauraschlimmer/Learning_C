a3cfb249aff72b9591ddeac818c3de1e1d1c6b0c
Laura Schlimmer <laura@etat-gray.com>
2014-10-19 13:44:59 +0100

    traversing & inserting

--- dlinked_list.c
+++ dlinked_list.c
@@ -26,8 +26,57 @@ struct list_s {
 
 typedef struct list_s list_t;
 
+void init(list_t* list) {
+  list->first = NULL;
+  list->last = NULL;
+  list->size = 0;
+}
+
+node_t* create_node() {
+  node_t* node = malloc(sizeof(node_t));
+  memset(node, 0, sizeof(node_t));
+  return node;
+}
+
+
+void insert_first(list_t* list, int value) {
+  node_t* node = create_node();
+  node->value = value;
+  
+  if (list->first == NULL) {
+    list->first = node;
+    list->last = node;
+    node->prev = NULL;
+    node->next = NULL;
+  } else {
+    node->next = list->first;
+    list->first->prev = node;
+    list->first = node;
+    node->prev = NULL;
+  }
+}
+
+void print_list_forwards(list_t* list) {
+  for (node_t* node = list->first; node != NULL; node = node->next) {
+    printf("value: %i\n", node->value);
+  }
+}
+
+void print_list_backwards(list_t* list) {
+  for (node_t* node = list->last; node != NULL; node = node->prev) {
+    printf("value: %i\n", node->value);
+  }
+}
+
+
+
 
 int main() {
+  list_t list;
+  init(&list);
+  insert_first(&list, 5);
+  print_list_forwards(&list);
+  print_list_backwards(&list);
   return 0;
 }
 
