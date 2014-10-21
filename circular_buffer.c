a1321e614843b9695732f6eb4d9df8c79c08aa5a
Laura Schlimmer <laura@etat-gray.com>
2014-10-21 12:04:03 +0100

    fix overwriting

--- circular_buffer.c
+++ circular_buffer.c
@@ -48,7 +48,11 @@ int isFull(cbuffer_t* buffer) {
 */
 void write(cbuffer_t* buffer, int value) {
   buffer->slots[buffer->end].value = value;
-  buffer->end = (buffer->end + 1) % buffer->size;
+  if (isFull(buffer)) {
+    buffer->start = (buffer->start + 1) % buffer->size;
+  }
+
+  buffer->end = (buffer->end +1) % buffer->size;
 }
 
 
@@ -69,11 +73,14 @@ void freeBuffer(cbuffer_t* buffer) {
 
 int main() {
   cbuffer_t cb;
-  init(&cb, 7);
+  init(&cb, 3);
   printf("is empty: %i\n", isEmpty(&cb));
   printf("is full: %i\n", isFull(&cb));
   write(&cb, 4);
-  slot_t* slot = read(&cb);
-  printf("value: %i\n", slot->value);
+  write(&cb, 5);
+  write(&cb, 2);
+  slot_t* slot1 = read(&cb);
+  slot_t* slot2 = read(&cb);
+  printf("first value: %i\n, second value: %i\n", slot1->value, slot2->value);
   return 0;
 }
